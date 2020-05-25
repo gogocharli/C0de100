37f02459f5156c1da77e8dea915bf83c744c3912
Charles De Mount <thexrayone@icloud.com>
2020-05-25 00:26:10 -0400

    Refactor beyonce.js to replace the api wrapper

--- src/_data/beyonce.js
+++ src/_data/beyonce.js
@@ -1,30 +1,51 @@
+// Require environment variables for API keys
+require('dotenv').config();
+
+// Get Base64 encoder
+const Base64 = require('js-base64').Base64;
+const fetch = require('node-fetch');
+
+const beyonceId = '6vWDO969PvNqNYHIOW5v0m';
+
 // TODO Query all the songs attributed to Beyoncé
-
-const SpotifyWebApi = require('spotify-web-api-node');
-
-// * Connect to the Spotify server and request an access token
-const spotifyApi = new SpotifyWebApi({
-  clientId: '0c9e398e8f2f4e79b65220a6e57ef524',
-  clientSecret: '950a17de5de946feb605ea806b479dae',
-});
-
-// Using the Client Credentials auth flow with the api wrapper
-spotifyApi
-  .clientCredentialsGrant()
-  .then(
-    data => {
-      spotifyApi.setAccessToken(data.body['access_token']);
+async function getToken() {
+  const res = await fetch('https://accounts.spotify.com/api/token', {
+    method: 'POST',
+    headers: {
+      Authorization: `Basic ${Base64.encode(
+        process.env.SPOTIFY_CLIENT_ID + ':' + process.env.SPOTIFY_CLIENT_SECRET
+      )}`,
+      'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8',
     },
-    err => {
-      console.log(
-        'And I OOP 😬! Something went wrong with retrieving an access token',
-        err.message
-      );
-    }
-  )
-  .then(() => {
-    spotifyApi.searchTracks('Beyonce').then(data => {
-      console.log(data.body.tracks.items);
-      return data.body.tracks.items;
-    });
+    body: 'grant_type=client_credentials',
   });
+  const tokenResponse = await res.json();
+  return tokenResponse['access_token'];
+}
+
+// The offset to call the api from in order to get enough tracks
+let offset = 0;
+async function getTracks(token) {
+  console.log(token);
+  const res = await fetch(
+    `https://api.spotify.com/v1/search?q=Beyonce&type=track&market=US&limit=50&offset=${offset}`,
+    {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  );
+  const data = await res.json();
+  console.log(data.tracks.items);
+}
+
+// Map the data to an object I can work with. While recieving an array of the tracks from that artist
+function mapData(tracks = []) {
+  for (let track of tracks) {
+    // Check if the Beyonce ID matches with one of the artists
+    // Get the Spotify ID for the track and push it into an array of Beyoncé tracks
+  }
+}
+
+getToken().then(token => getTracks(token));
