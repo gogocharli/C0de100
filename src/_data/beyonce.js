0a0aa67bd3ba46ceaec4fceeee1fec9d04f7d7f1
Charles De Mount <thexrayone@icloud.com>
2020-05-25 23:16:46 -0400

    Process track IDs and export to tracks.json

--- src/_data/beyonce.js
+++ src/_data/beyonce.js
@@ -7,7 +7,7 @@ const fetch = require('node-fetch');
 
 const beyonceId = '6vWDO969PvNqNYHIOW5v0m';
 
-// TODO Query all the songs attributed to Beyoncé
+// Get the access token using the client credentials auth flow
 async function getToken() {
   const res = await fetch('https://accounts.spotify.com/api/token', {
     method: 'POST',
@@ -23,29 +23,54 @@ async function getToken() {
   return tokenResponse['access_token'];
 }
 
-// The offset to call the api from in order to get enough tracks
-let offset = 0;
-async function getTracks(token) {
-  console.log(token);
-  const res = await fetch(
-    `https://api.spotify.com/v1/search?q=Beyonce&type=track&market=US&limit=50&offset=${offset}`,
-    {
-      method: 'GET',
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    }
-  );
-  const data = await res.json();
-  console.log(data.tracks.items);
+// Search for tracks belonging to the artist
+async function getTracks(token, offsetMax = 1) {
+  let tracks = [];
+
+  // Since the API limit for this query is 50 results, I am making it multiple times to get enouth items
+  let offset = 0;
+  for (let i = 0; i < offsetMax; i++) {
+    const res = await fetch(
+      `https://api.spotify.com/v1/search?q=Beyonce&type=track&market=US&limit=50&offset=${offset}`,
+      {
+        method: 'GET',
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+    const data = await res.json();
+
+    // Use the old tracks array and append the newly recieved set of results, then flatten it
+    tracks = await [tracks, ...data.tracks.items].flat();
+    offset += 50;
+  }
+  return tracks;
 }
 
-// Map the data to an object I can work with. While recieving an array of the tracks from that artist
+// Map the data to an object I can work with. While recieving an array of the tracks
 function mapData(tracks = []) {
+  const trackIDs = [];
+  // Tracks is an array of IDs of the different Beyonce tracks as IDs
   for (let track of tracks) {
     // Check if the Beyonce ID matches with one of the artists
-    // Get the Spotify ID for the track and push it into an array of Beyoncé tracks
+    for (let { id } of track.artists) {
+      if (id == beyonceId) trackIDs.push(track.id);
+    }
   }
+  return trackIDs;
 }
 
-getToken().then(token => getTracks(token));
+async function getBeyonce() {
+  const token = await getToken();
+  const tracks = await getTracks(token, 4);
+  const beyonceTracks = await mapData(tracks);
+  console.log(
+    'Node caters to you! 😌 I have successfuly retrieved',
+    beyonceTracks.length,
+    'Beyoncé tracks'
+  );
+  return beyonceTracks;
+}
+
+module.exports = getBeyonce();
